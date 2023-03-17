76673b7b68db9e03a9bc1608cad6d2564f51b042
Achteins-81 <fyx1993@126.com>
2023-03-17 15:04:45 +0800

    <feat>(Java-Basic):BaseConstant常量类新增一些常量，如拼写字母、月份、星座

--- Java-Basic/src/main/java/org/achteins81/app/base/constant/BaseConstant.java
+++ Java-Basic/src/main/java/org/achteins81/app/base/constant/BaseConstant.java
@@ -21,6 +21,17 @@ public class BaseConstant {
     public static final String SPECIAL_CHARACTER_STRING = "~!@#$%^&*()_+/-=[]{};'<>?.";
 
     /**
+     * 国际无线电通话拼写字母，(International) Radiotelephony Spelling Alphabet
+     *
+     * @see <a href="https://en.wikipedia.org/wiki/NATO_phonetic_alphabet">source1</a>
+     * and
+     * <a href="https://baike.baidu.com/item/%E5%8C%97%E7%BA%A6%E9%9F%B3%E6%A0%87%E5%AD%97%E6%AF%8D/4157760">source2</a>
+     */
+    public static final String[] RADIOTELEPHONY_SPELLING_ALPHABET = {"Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot",
+            "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo",
+            "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu"};
+
+    /**
      * 天干
      */
     public static final String HEAVENLY_STEMS = "甲乙丙丁戊己庚辛壬癸";
@@ -36,4 +47,61 @@ public class BaseConstant {
      * 地支，数组形式
      */
     public static final String[] EARTHLY_BRANCHES_ARRAY = EARTHLY_BRANCHES.split("");
+
+    /**
+     * 月份 in English
+     */
+    public static final String[] MONTHS_ENGLISH = {"January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"};
+
+    /**
+     * 月份 auf Deutsch
+     */
+    public static final String[] MONATE_DEUTSCH = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli",
+            "August", "September", "Oktober", "November", "Dezember"};
+
+    /**
+     * 星座
+     * <br/>
+     * <table>
+     * <tr>
+     * ♈	白羊座	Aries
+     * </tr>
+     * <tr>
+     * ♉	金牛座	Taurus
+     * </tr>
+     * <tr>
+     * ♊	双子座	Gemini
+     * </tr>
+     * <tr>
+     * ♋	巨蟹座	Cancer
+     * </tr>
+     * <tr>
+     * ♌	狮子座	Leo
+     * </tr>
+     * <tr>
+     * ♍	处女座	Virgo
+     * </tr>
+     * <tr>
+     * ♎	天秤座	Libra
+     * </tr>
+     * <tr>
+     * ♏	天蝎座	Scorpio
+     * </tr>
+     * <tr>
+     * ♐	射手座	Sagittarius
+     * </tr>
+     * <tr>
+     * ♑	摩羯座	Capricorn
+     * </tr>
+     * <tr>
+     * ♒	水瓶座	Aquarius
+     * </tr>
+     * <tr>
+     * ♓	双鱼座	Pisces
+     * </tr>
+     * </table>
+     */
+    public static final String[] TWELVE_CONSTELLATIONS = {"Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo",
+            "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"};
 }
